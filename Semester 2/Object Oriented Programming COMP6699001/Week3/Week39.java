f7be7373e753ff87cba725accdcf6f88e80b606e
Own20 <christopherowen2000403@gmail.com>
2022-03-05 00:47:45 +0700

    Create Week39.java

---
+++ Semester 2/Object Oriented Programming COMP6699001/Week3/Week39.java
@@ -0,0 +1,12 @@
+// number 9
+
+public class Week39 {
+    public static void main(String[] args) {
+        int c = 20;
+        int celciusToFahrenheit;
+
+        celciusToFahrenheit = (c / 5 * 9) + 32;
+
+        System.out.println(c + " Celcius degrees are " + celciusToFahrenheit + " Fahrenheit degrees.");
+    }
+}
