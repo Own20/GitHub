6a0fe64715eda538e7588cb9274cd78e1361be56
Own20 <christopherowen2000403@gmail.com>
2022-03-05 00:47:39 +0700

    Create Week311.java

---
+++ Semester 2/Object Oriented Programming COMP6699001/Week3/Week311.java
@@ -0,0 +1,14 @@
+// number 11
+
+public class Week311 {
+    public static void main(String[] args) {
+        String x = "10";  
+        String y = "5";
+        
+        String replaceNum = x.replace("10","5");
+        String replaceNum0 = y.replace("5", "10");
+
+        System.out.println(replaceNum);
+        System.out.println(replaceNum0);
+    }
+}
