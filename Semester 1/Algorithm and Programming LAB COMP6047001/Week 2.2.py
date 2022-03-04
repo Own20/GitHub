5932608936154f5277bc26773580af46ba95bca3
Own20 <christopherowen2000403@gmail.com>
2022-03-05 00:46:47 +0700

    Create Week 2.2.py

---
+++ Semester 1/Algorithm and Programming LAB COMP6047001/Week 2.2.py
@@ -0,0 +1,39 @@
+"""
+li = []
+li2 = ['hello', 'there']
+        #0          #1
+li3 = [['hello', 'there'], ['general', 'kenobi']]
+
+print(li2[0])
+print(li3[0][1])
+"""
+###
+"""
+strings = 'hello hello, koko ni iru yo'
+        #  0123456789
+print(strings[0])
+print(strings[0:5])
+print(strings[13:27])
+print(len(strings))
+a = strings[13:-5]
+print(a)
+b = strings[:13]
+print(b)
+"""
+###
+"""
+li = ["hello", "there", "general", "spongebob", "squarepants"]
+print(li[0:3])
+"""
+###
+"""
+I have a link
+chu2.tech/102389/asfhadsfha$1
+0123456789012345678901234567890
+asfhadsfha
+"""
+string = 'chu2.tech/102389/asfhadsfha$1'
+print(string[17:-2])
+print(string[17:27])
+
+
