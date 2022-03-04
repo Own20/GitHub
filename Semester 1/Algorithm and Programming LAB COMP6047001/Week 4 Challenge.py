0212f98ebf7b5c73fe74df40a82727b101d389e9
Own20 <christopherowen2000403@gmail.com>
2022-03-05 00:46:54 +0700

    Create Week 4 Challenge.py

---
+++ Semester 1/Algorithm and Programming LAB COMP6047001/Week 4 Challenge.py
@@ -0,0 +1,13 @@
+I want you to print out a chessboard
+Then a user can input a string, let's say C4
+That C4 acts as a knight
+You have a list of [A, B, C, D, E, F, G, H] and [1, 2, 3, 4, 5, 6, 7, 8]`
+
+A8 B8 C8 D8 E8 F8 G8 H8
+A7 B7 C7 D7 E7 F7 G7 H7
+A6 xx C6 xx E6 F6 G6 H6
+xx B5 C5 D5 xx F5 G5 H5
+A4 B4 kn D4 E4 F4 G4 H4
+xx B3 C3 D3 xx F3 G3 H3
+A2 xx C2 xx E2 F2 G2 H2
+A1 B1 C1 D1 E1 F1 G1 H1
