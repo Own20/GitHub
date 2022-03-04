68ffd870fd1b2cf928e272ec8ddc4014798111cd
Own20 <christopherowen2000403@gmail.com>
2022-03-05 00:47:46 +0700

    Create Week38.java

---
+++ Semester 2/Object Oriented Programming COMP6699001/Week3/Week38.java
@@ -0,0 +1,15 @@
+// number 8
+
+public class Week38 {
+    public static void main(String[] args) {
+        System.out.println("*********     ***       *        *    ");
+        System.out.println("*       *   *     *    ***      * *   ");
+        System.out.println("*       *  *       *  *****    *   *  ");
+        System.out.println("*       *  *       *    *     *     * ");
+        System.out.println("*       *  *       *    *    *       *");
+        System.out.println("*       *  *       *    *     *     * ");
+        System.out.println("*       *  *       *    *      *   *  ");
+        System.out.println("*       *   *     *     *       * *   ");
+        System.out.println("*********     ***       *        *    ");
+    }
+}
