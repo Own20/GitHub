36c8311d109b65d50dd4df97d2eec053b0b06203
Own20 <christopherowen2000403@gmail.com>
2022-03-05 00:46:43 +0700

    Create Week 2.0.py

---
+++ Semester 1/Algorithm and Programming LAB COMP6047001/Week 2.0.py
@@ -0,0 +1,42 @@
+"""
+#1
+age = int(input("Insert your age here: "))
+age2 = int(input("Insert your spouse's age here: "))
+print(age + age2)
+
+#2
+age = input("Insert your age here: ")
+age2 = input("Insert your spouse's age here: ")
+age = int(age)
+age2 = int(age2)
+print(age + age2)
+
+#3
+age = int(input("Insert your age here: "))
+age2 = int(input("Insert your spouse's age here: "))
+print(int(age) + int(age2))
+
+#4
+age = eval(input("Insert your age here: "))
+age2 = eval(input("Insert your spouse's age here: "))
+print(age + age2)
+
+#5
+age = (input("Insert your age here: "))
+age2 = (input("Insert your spouse's age here: "))
+print("My age is" + age + "and my spouse's age is" + age2)
+
+#6
+age = (input("Insert your age here: "))
+age2 = (input("Insert your spouse's age here: "))
+print("My age is" + age + "and my spouse's age is" + age2)
+
+#7
+age = (input("Insert your age here: "))
+age2 = (input("Insert your spouse's age here: "))
+print(f"My age is {age} and my spouse's age is {age2}")
+"""
+#8
+age = (input("Insert your age here: "))
+age2 = (input("Insert your spouse's age here: "))
+print("My age is {} and my spouse's age is {}". format(age, age2))
