295e5e0101dcd3f8ed5b4691cab147b78953065c
Own20 <christopherowen2000403@gmail.com>
2022-03-25 23:07:57 +0700

    Create Week 1 Assignment.py

---
+++ Semester 1/Algorithm and Programming LEC COMP6047001/Week 1 Assignment.py
@@ -0,0 +1,23 @@
+print ("hi", end="\n")
+print ("low")
+
+print ("hi", end=" ")
+print ("low")
+
+print ("hi", end=" ")
+print ("low")
+print ("high", end="**")
+print ("lowe")
+
+print ("hi","low","high","lowe",sep="   ")  # separator
+print ("bye","hello",end= "*",sep="#")
+
+"""
+this is a multi line comment
+hi there
+"""
+'''
+this is a multi line comment
+hi there
+'''
+#this is a single comment
