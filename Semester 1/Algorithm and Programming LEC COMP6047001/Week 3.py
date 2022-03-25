e717b1831ed5d1d833b7cb43c5a6aff319053f51
Own20 <christopherowen2000403@gmail.com>
2022-03-25 23:10:26 +0700

    Create Week 3.py

---
+++ Semester 1/Algorithm and Programming LEC COMP6047001/Week 3.py
@@ -0,0 +1,19 @@
+Topic for today:
+    1.  While loop
+        syntax:
+            while <test_expr> :
+                <body>
+        finite and infinite loops, break, continue, pass
+    2.  Lists (mutable) --> []
+        Creating a list
+        Indexing
+        Length of list
+        Assignment
+        Some useful functions : append(), extend(), insert(index,val),
+                                remove(value or index), min(), max(),
+                                sum(), sort(), islower(), isupper()
+    3.  for loop
+        syntax:
+            for <item> in <iterable>:
+                <body>
+    4.  range([start], stop, [increment])
