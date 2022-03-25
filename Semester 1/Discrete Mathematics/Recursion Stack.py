b0f2bef34fc031975b90096d0aa9c9dad64dc6e7
Own20 <christopherowen2000403@gmail.com>
2022-03-25 23:20:50 +0700

    Create Recursion Stack.py

---
+++ Semester 1/Discrete Mathematics/Recursion Stack.py
@@ -0,0 +1,20 @@
+# def power(a, n):
+#     if n == 0:
+#         p = 1
+#     else:
+#         p = a * power(a, n - 1)
+#     return p
+
+# print(power(2, 5))
+
+#Fibonacci
+if n = 0:
+    return 0
+else:
+    x := 0
+    y := 1
+    for i :=1 to n - 1:
+        z:= x + y
+        x := y
+        y := z
+        
