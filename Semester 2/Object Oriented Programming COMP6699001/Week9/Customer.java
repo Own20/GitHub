4c1dabc2f0f47be379c4e08357b6d997a330e1db
Own20 <christopherowen2000403@gmail.com>
2022-05-11 00:28:33 +0700

    Create Customer.java

---
+++ Semester 2/Object Oriented Programming COMP6699001/Week9/Customer.java
@@ -0,0 +1,29 @@
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private Account account;
+
+    public Customer(String f, String l) {
+        this.firstName = f;
+        this.lastName = l;
+    }
+    public Customer(String f, String l, Account account) {
+        this.firstName = f;
+        this.lastName = l;
+        this.account = account;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+}
