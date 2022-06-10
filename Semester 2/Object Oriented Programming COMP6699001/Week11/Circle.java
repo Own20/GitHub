fb31bc7867ecac0e4d482691da9b57d96ed5926c
Own20 <christopherowen2000403@gmail.com>
2022-06-11 00:57:14 +0700

    Create Circle.java

---
+++ Semester 2/Object Oriented Programming COMP6699001/Week11/Circle.java
@@ -0,0 +1,40 @@
+package Week11;
+
+public class Circle extends Shape implements GeometricObject {
+    protected double radius;
+
+    public Circle() {
+        this.radius = 1.0;
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return Math.PI * 2 * this.radius;
+    }
+
+    public String toString() {
+        return "Circle{" + "radius = " + radius + "}" + super.toString();
+    }
+}
