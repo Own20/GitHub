579bd1fd97f40ffc864bc8b5d0f5a9901121a74d
Own20 <christopherowen2000403@gmail.com>
2022-06-11 00:54:47 +0700

    Delete member-search-index.js

--- Semester 2/Object Oriented Programming COMP6699001/Student_Result_Recording_System/target/site/apidocs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"com.mycompany.student_result_recording_system","c":"Student_Result","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"com.mycompany.student_result_recording_system","c":"Student_Result","l":"Student_Result()","u":"%3Cinit%3E()"}];updateSearchResults();
