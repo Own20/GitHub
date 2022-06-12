670413f69e48c51377fcb6f533930f76454581cf
Own20 <christopherowen2000403@gmail.com>
2022-06-13 01:05:22 +0700

    Create Test0Main.java

---
+++ Semester 2/Object Oriented Programming COMP6699001/Test0Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Test0Main {
+    private Test0Numbers number;
+
+   //example
+    public static void main(String[] args) {
+        number = new Test0Numbers();
+        ArrayList<Integer> list = number.getList();
+        //hurray !
+    }
+}
